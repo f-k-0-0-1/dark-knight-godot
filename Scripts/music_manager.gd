8792feebc80bf4d18681dadc3c0eb15ae27e247a
Sami <physicskabaap2025@gmail.com>
2026-06-25 20:56:47 +0530

    Added Background Music Mute Option

--- Scripts/music_manager.gd
+++ Scripts/music_manager.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
+#static var 
+static var isMusicPlaying: bool 
+
 @onready var music: AudioStreamPlayer = $AudioStreamPlayer
 @onready var sfx: AudioStreamPlayer = $GameOver
 @onready var button_click: AudioStreamPlayer = $buttonclick
 
 func _ready():
 	music.play()
+	isMusicPlaying = true
 	
 func play_game_over():
 	sfx.stream = preload("res://Audio/sfx/game over.mp3")
