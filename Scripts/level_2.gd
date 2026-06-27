7b941acb49d7d8669226b5320b6e5e5b1c5f91d9
faiqbuilds <khanfaiq264@gmail.com>
2026-06-27 17:43:38 +0530

    added better tutorial visuals

--- Scripts/level_2.gd
+++ Scripts/level_2.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	tutorial_instance = tutorial_scene.instantiate()
 	add_child(tutorial_instance)
 	tutorial_instance.show_tutorial(
-		"Welcome To Level 2\nTUTORIAL:\nPress A/D to move\nPress W or Space to jump\nPress F or Enter to shoot\nPress G to toggle God Mode"
+		"Press A/D to move\nPress W or Space to jump\nPress F or Enter to shoot\nPress G to toggle God Mode"
 	)
 	
 	tutorial_instance.tutorial_closed.connect(_on_tutorial_closed)
